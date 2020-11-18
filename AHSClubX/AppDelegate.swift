52597928ff91e8f75ba02885eda272b27f408c06
Urvi Bhuwania <urvibhuwania25@gmail.com>
2020-11-17 19:01:22 -0800

    GoogleSignIn

--- AHSClubX/AppDelegate.swift
+++ AHSClubX/AppDelegate.swift
@@ -6,31 +6,81 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+@UIApplicationMain
+// [START appdelegate_interfaces]
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
+  // [END appdelegate_interfaces]
+  var window: UIWindow?
 
+  // [START didfinishlaunching]
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Initialize sign-in
+    GIDSignIn.sharedInstance().clientID = "716005235665-9aqbhlnbk7j6ee5lu8m30ku6s74vpec2.apps.googleusercontent.com"
+    GIDSignIn.sharedInstance().delegate = self
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    return true
+  }
+  // [END didfinishlaunching]
+
+  // [START openurl]
+  func application(_ application: UIApplication,
+                   open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    return GIDSignIn.sharedInstance().handle(url)
+  }
+  // [END openurl]
+
+  // [START openurl_new]
+  @available(iOS 9.0, *)
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+    return GIDSignIn.sharedInstance().handle(url)
+  }
+  // [END openurl_new]
+
+  // [START signin_handler]
+  func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+            withError error: Error!) {
+    if let error = error {
+      if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+        print("The user has not signed in before or they have since signed out.")
+      } else {
+        print("\(error.localizedDescription)")
+      }
+      // [START_EXCLUDE silent]
+      NotificationCenter.default.post(
+        name: Notification.Name(rawValue: "ToggleAuthUINotification"), object: nil, userInfo: nil)
+      // [END_EXCLUDE]
+      return
     }
+    // Perform any operations on signed in user here.
+    let userId = user.userID                  // For client-side use only!
+    let idToken = user.authentication.idToken // Safe to send to the server
+    let fullName = user.profile.name
+    let givenName = user.profile.givenName
+    let familyName = user.profile.familyName
+    let email = user.profile.email
+    // [START_EXCLUDE]
+    NotificationCenter.default.post(
+      name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+      object: nil,
+      userInfo: ["statusText": "Signed in user:\n\(fullName!)"])
+    // [END_EXCLUDE]
+  }
+  // [END signin_handler]
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+  // [START disconnect_handler]
+  func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+            withError error: Error!) {
+    // Perform any operations when the user disconnects from app here.
+    // [START_EXCLUDE]
+    NotificationCenter.default.post(
+      name: Notification.Name(rawValue: "ToggleAuthUINotification"),
+      object: nil,
+      userInfo: ["statusText": "User has disconnected."])
+    // [END_EXCLUDE]
+  }
+  // [END disconnect_handler]
 }
-
